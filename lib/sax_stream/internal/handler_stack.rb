7321c0b5b5d45e533c088230eb5c5cc1dd31c01d
Craig Ambrose <craig@craigambrose.com>
2012-04-05 14:18:36 +1000

    handler stack works

--- lib/sax_stream/internal/handler_stack.rb
+++ lib/sax_stream/internal/handler_stack.rb
@@ -2,11 +2,21 @@ module SaxStream
   module Internal
     class HandlerStack
       def initialize(root_handler)
-        @root = root_handler
+        raise ArgumentError unless root_handler
+        @handlers = [root_handler]
       end
 
       def top
-        @root
+        @handlers.last
+      end
+
+      def push(handler)
+        @handlers.push(handler)
+      end
+
+      def pop
+        raise ProgramError, "can't push the last handler" if @handlers.length <= 1
+        @handlers.pop
       end
     end
   end
